56aee4eb41ec5c485a2b9b8a050f5a471b1399eb
Long Tran <longtran@Longs-MacBook-Pro.local>
2022-07-28 17:08:29 +0700

    debug theme in consumer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -11,18 +11,24 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
+    var appSecondaryStyle = Theme.of(context).colorScheme.secondary;
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Product>(
+            builder: ((context, value, child) {
+              return IconButton(
+                icon: Icon(product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () {
+                  product.toggleFavoriteStatus();
+                },
+                color: appSecondaryStyle, // Vì context ko phải của app nữa
+              );
+            }),
           ),
           trailing: IconButton(
             onPressed: () {},
