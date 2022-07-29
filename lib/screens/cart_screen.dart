dd029271e97875f45b6e074b4b391b1a96def6a7
Long Tran <longtran@Longs-MacBook-Pro.local>
2022-07-29 14:31:50 +0700

    display cart item

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -47,7 +49,21 @@ class CartScreen extends StatelessWidget {
                 ],
               ),
             ),
-          )
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: ((context, index) => CartItem(
+                    cart.items.values.toList()[index].id,
+                    cart.items.values.toList()[index].price,
+                    cart.items.values.toList()[index].quantity,
+                    cart.items.values.toList()[index].title,
+                  )),
+              itemCount: cart.items.length,
+            ),
+          ),
         ],
       ),
     );
