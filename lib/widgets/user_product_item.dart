674aa5fca2d55124cbbe5228d56b615af0aeb9ec
Long Tran <longtran@192.168.2.21>
2022-08-01 15:56:49 +0700

    delete product

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
   const UserProductItem(
@@ -37,7 +39,28 @@ class UserProductItem extends StatelessWidget {
               ),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: const Text("Are you sure ?"),
+                  content: const Text("Do you want to remove this item ?"),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("No"),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Provider.of<Products>(context, listen: false)
+                              .deleteProduct(id);
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("Yes"))
+                  ],
+                ),
+              ),
               icon: Icon(
                 Icons.delete,
                 color: Theme.of(context).errorColor,
