f3b4de17f3baad30e78a600322fb75cea3f98fef
Long Tran <longtran@192.168.2.21>
2022-08-04 10:50:21 +0700

    orders transtition

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -21,29 +21,34 @@ class _OrderItemState extends State<OrderItem> {
   bool _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            subtitle: Text(
-              DateFormat('dd-MM-yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 400),
+      height:
+          _expanded ? min(widget.order.products.length * 20 + 110, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              subtitle: Text(
+                DateFormat('dd-MM-yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 5),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 400),
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20 + 10, 100),
+              height: _expanded
+                  ? min(widget.order.products.length * 20 + 10, 100)
+                  : 0,
               child: ListView.builder(
                 itemBuilder: (context, index) {
                   return Row(
@@ -69,7 +74,8 @@ class _OrderItemState extends State<OrderItem> {
                 itemCount: widget.order.products.length,
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
